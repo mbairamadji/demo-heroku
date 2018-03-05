6d92edc18b81ba79e5e4cb1b2d5e33e60cad6aeb
MBAIRAMADJI <mbairamadjieric@gmail.com>
2018-03-05 16:18:40 +0100

    deploy heroku

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use('/', express.static(__dirname + '/front/build'))
+app.use(express.static(__dirname + '/front/build'))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/front/build/index.html')
-})
+// app.get('/', (req, res) => {
+//     res.send(__dirname + '/front/build/index.html')
+// })
 
 app.listen(port, () => {
     console.log("App listening on 3000")
